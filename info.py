55ba2d15ec657e8585721aedbec5aa048f0e4841
satyaakushwah2003-lab <satyaakushwah2003@gmail.com>
2026-04-17 15:52:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -165,7 +165,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://empirical-ralina-filmydoses-8857935c.koyeb.app/")
+URL = environ.get("URL", "https://animedoses.blogspot.com/p/s_16.html")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
